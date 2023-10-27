e2e033b08ef7895824ccaadb8fc835f23d30d0a1
lunamoonmoon <5862lunamoon@gmail.com>
2023-10-27 16:06:16 -0700

    incl componentdidmount and photo api

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,7 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 
 import classnames from "classnames";
+import { getTotalPhotos, getTotalTopics, getUserWithLeastUploads, getUserWithMostUploads } from "helpers/selectors";
 import Loading from "./Loading";
 import Panel from "./Panel";
 
@@ -8,35 +9,64 @@ const data = [
   {
     id: 1,
     label: "Total Photos",
-    value: 10
+    getValue: getTotalPhotos
   },
   {
     id: 2,
     label: "Total Topics",
-    value: 4
+    getValue: getTotalTopics
   },
   {
     id: 3,
     label: "User with the most uploads",
-    value: "Allison Saeng"
+    getValue: getUserWithLeastUploads
   },
   {
     id: 4,
     label: "User with the least uploads",
-    value: "Lukas Souza"
+    getValue: getUserWithMostUploads
   }
 ];
 
 class Dashboard extends Component {
   state = {
-    loading: false,
+    photos: [],
+    topics: [],
+    loading: true,
     focused: null
   };
 
-  selectPanel(id) {
-    this.setState({
-     focused: id
+  componentDidMount() {
+    const urlsPromise = [
+      "/api/photos",
+      "/api/topics",
+    ].map(url => fetch(url).then(res => res.json()));
+
+    Promise.all(urlsPromise)
+    .then(([photos, topics]) => {
+      this.setState({
+        loading: false,
+        photos: photos,
+        topics: topics
+      });
     });
+
+    const focused = JSON.parse(localStorage.getItem("focused"));
+    if (focused) {
+      this.setState({focused});
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  selectPanel(id) {
+    this.setState(prevState => ({
+     focused: prevState.focused !== null ? null : id
+    }));
    }
 
   render() {
@@ -52,10 +82,9 @@ class Dashboard extends Component {
     .map(panel => (
      <Panel
       key={panel.id}
-      id={panel.id}
       label={panel.label}
-      value={panel.value}
-      onSelect={this.selectPanel}
+      value={panel.getValue(this.state)}
+      onSelect={() => this.selectPanel(panel.id)}
      />
     ));
 
